c9524b40a500204fbbf40015b79a504c0d2f3cd5
Bruno Mourinha <brmourinha@gmail.com>
2020-02-06 15:19:18 +0000

    GraphQL config

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,18 +1,88 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
+//dummy data
+let wines = [
+  { id: '1', name: 'Pias', type: 'Branco', producerId: '1' },
+  {
+    id: '2',
+    name: 'EA',
+    type: 'Tinto',
+    producerId: '2'
+  },
+  {
+    id: '3',
+    name: 'Ponte de Lima',
+    type: 'Verde',
+    producerId: '3'
+  },
+  {
+    id: '4',
+    name: 'EA',
+    type: 'Branco',
+    producerId: '2'
+  },
+  {
+    id: '5',
+    name: 'Vinea',
+    type: 'Tinto',
+    producerId: '2'
+  }
+];
+
+let producers = [
+  { id: '1', name: 'Adega Pias', region: 'Alentejo', createdYear: 1980 },
+  { id: '2', name: 'Cartuxa', region: 'Alentejo', createdYear: 1975 },
+  { id: '3', name: 'Ponte Lima', region: 'Minho', createdYear: 1950 }
+];
+
+// Wine Type
 const WineType = new GraphQLObjectType({
   name: 'Wine',
   fields: () => ({
     id: {
-      type: GraphQLString
+      type: GraphQLID
     },
     name: { type: GraphQLString },
     type: {
       type: GraphQLString
     },
-    producer: { type: GraphQLString }
+    producer: {
+      type: ProducerType,
+      resolve(parent, args) {
+        return _.find(producers, { id: parent.producerId });
+      }
+    }
+  })
+});
+
+// Producer
+const ProducerType = new GraphQLObjectType({
+  name: 'Producer',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    name: { type: GraphQLString },
+    region: {
+      type: GraphQLString
+    },
+    createdYear: { type: GraphQLInt },
+    wines: {
+      type: new GraphQLList(WineType),
+      resolve(parent, args) {
+        return _.filter(wines, { producerId: parent.id });
+      }
+    }
   })
 });
 
@@ -21,9 +91,30 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     wine: {
       type: WineType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // Code to get data from db / other source
+        return _.find(wines, { id: args.id });
+      }
+    },
+    producer: {
+      type: ProducerType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // Code to get data from db / other source
+        return _.find(producers, { id: args.id });
+      }
+    },
+    wines: {
+      type: new GraphQLList(WineType),
+      resolve(parent, args) {
+        return wines;
+      }
+    },
+    producers: {
+      type: new GraphQLList(ProducerType),
+      resolve(parent, args) {
+        return producers;
       }
     }
   }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongosse = require('mongoose');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to Database
+connectDB();
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running at port:${PORT}`);
+  console.log(`Server is running at port:${PORT}`.bgBlue.black);
 });
